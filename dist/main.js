9235a0e7cee22a41e344ff271a79cf2e516a3d9c
Avishay Peres <avishay.peres@gmail.com>
2019-04-17 16:21:32 +0300

    added pending card request

--- dist/main.js
+++ dist/main.js
@@ -36,7 +36,7 @@ $(".openBetsMenu").find(".team-btn").on("click", function () {
         console.log(saveTeam2)
  
     let saveBetCard = { user1: saveUser1, team1: saveTeam1, user2: saveUser2, team2: saveTeam2 }
-    $.post('/betcards', saveBetCard, function (response) {
+    $.post('/openbetcards', saveBetCard, function (response) {
         console.log(saveBetCard)
     })
 })
